d693d0091f9c0f1857521f809369f2e35761e710
PinkPic <77137417+PinkPicOfficial@users.noreply.github.com>
2022-01-24 14:21:08 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const {
+    Client,
+    Intents
+} = require('discord.js');
+const {
+    bot_token,
+    status
+} = require('./misc/config.json');
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+client.util = require('./util');
+
+client.on('warn', err => console.warn('[WARNING]', err));
+
+client.on('error', err => console.error('[ERROR]', err));
+
+client.on('disconnect', () => {
+    console.warn('Disconnected!')
+    process.exit(0);
+});
+
+client.on('uncaughtException', (err) => {
+    console.log('Uncaught Exception: ' + err)
+    process.exit(1)
+});
+
+client.on('messageCreate', (msg) => {
+    if (msg.author.bot) return;
+    if (msg.guild) {
+        if (msg.content.startsWith(`<@${msg.client.user.id}>`) || msg.content.startsWith(`<@!${msg.client.user.id}>`)) {
+            client.util.handleTalk(msg);
+        }
+    }
+});
+
+client.on('ready', () => {
+    client.util.handleStatus(client, status);
+    console.log('[CLEVE] Started and ready to chat!');
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.log('[FATAL] Possibly Unhandled Rejection at: Promise ', promise, ' reason: ', reason.message);
+});
+
+client.login(bot_token);
